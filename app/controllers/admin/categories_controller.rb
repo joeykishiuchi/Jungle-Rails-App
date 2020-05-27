a28932ac05ad3b331a8e99f75f79876754c115dc
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-27 21:28:18 +0000

    Added create path

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
   def index
-    @categories = Category.joins(:products).select('categories.*, COUNT(categories.name) AS total_products').group(:id).order(id: :desc)
+    @categories = Category.order(id: :desc).all
   end
 
   def new
@@ -11,5 +11,19 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save 
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else 
+      render :new
+    end
   end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
 end
