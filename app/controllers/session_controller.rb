b0fac515433457fd46f7bd6eb8fc79af2fa1695d
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-28 01:27:01 +0000

    Organized signup layout and styles

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,17 +7,17 @@ class SessionController < ApplicationController
     user = User.find_by_email(params[:email])
     # Checks if user exists and password entered is correct
     if (user && user.authenticate(params[:password]))
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       redirect_to '/'
     else
       # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login/new'
+      redirect_to '/login'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login/new'
+    redirect_to '/login'
   end
 
 end
