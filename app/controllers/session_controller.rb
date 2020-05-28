118c38d2efac06045d74172d8439f3bea1b41449
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-28 01:56:33 +0000

    Fixed login form submission

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,9 +4,9 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(user_params[:email])
     # Checks if user exists and password entered is correct
-    if (user && user.authenticate(params[:password]))
+    if (user && user.authenticate(user_params[:password]))
       session[:user_id] = user.id
       redirect_to '/'
     else
@@ -20,4 +20,8 @@ class SessionController < ApplicationController
     redirect_to '/login'
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
