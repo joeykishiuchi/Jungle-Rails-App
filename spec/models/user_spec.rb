26d6b85cdcacd54241a4a18ae1004fd6b85c4a85
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-06-01 21:38:26 +0000

    validates user name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe User, type: :model do
       })
       expect(@user.save).to eq(true)
     end
+    it "saves user with all necessary fields" do
+      @user = User.new({
+        :name => nil,
+        :email => 'john.smith@gmail.com',
+        :password => 'password',
+        :password_confirmation => 'password'
+      })
+      @user.save
+      expect(@user.errors.full_messages).to eq(["Name can't be blank"])
+    end
+
   end
 
   describe '.authenticate_with_credentials' do
