609404eb2dd8dfccd4d35523cd0d82a9aaad7dc2
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-06-01 20:54:00 +0000

    Run and passed tests for product validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,14 +8,54 @@ RSpec.describe Product, type: :model do
 
   describe 'Validations' do
     it 'saves product with all necessary fields' do
+      @product = Product.new({
+        :name => 'ball',
+        :price => 10,
+        :quantity => 10,
+        :category => @category
+      })
+      
+      expect(@product.save).to eq(true)
     end
     it 'returns error when :name is not given' do 
+      @product = Product.new({
+        :name => nil,
+        :price => 10,
+        :quantity => 10,
+        :category => @category
+      })
+      @product.save
+      expect(@product.errors.full_messages).to eq(["Name can't be blank"])
     end
-    it 'returns error when :price is not given' do 
+    it 'returns error when :price is not given' do
+      @product = Product.new({  
+        :name => 'ball',
+        :price => nil,
+        :quantity => 10,
+        :category => @category
+      })
+      @product.save
+      expect(@product.errors.full_messages).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
     end
-    it 'returns error when :quantity is not given' do 
+    it 'returns error when :quantity is not given' do
+      @product = Product.new({  
+        :name => 'ball',
+        :price => 10,
+        :quantity => nil,
+        :category => @category
+      }) 
+      @product.save
+      expect(@product.errors.full_messages).to eq(["Quantity can't be blank"])
     end
-    it 'returns error when :category is not given' do 
+    it 'returns error when :category is not given' do
+      @product = Product.new({  
+        :name => 'ball',
+        :price => 10,
+        :quantity => 10,
+        :category => nil
+      })
+      @product.save
+      expect(@product.errors.full_messages).to eq(["Category can't be blank"])
     end
   end
 end
