1ee11be57d4a505d04d4812d2a23b538894293e6
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-06-02 20:23:47 +0000

    Replaced all cases of :name with :first_name and :last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
-  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true
   validates_uniqueness_of :email, :case_sensitive => false
   validates :password, confirmation:true, length: { minimum: 8 }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,26 +4,29 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     it "saves user with all necessary fields" do
       @user = User.new({
-        :name => 'John Smith',
+        :first_name => 'John',
+        :last_name => 'Smith',
         :email => 'john.smith@gmail.com',
         :password => 'password',
         :password_confirmation => 'password'
       })
       expect(@user.save).to eq(true)
     end
-    it "should give error if :name is not given" do
+    it "should give error if :first_name and :last_name is not given" do
       @user = User.new({
-        :name => nil,
+        :first_name => nil,
+        :last_name => nil,
         :email => 'john.smith@gmail.com',
         :password => 'password',
         :password_confirmation => 'password'
       })
       @user.save
-      expect(@user.errors.full_messages).to eq(["Name can't be blank"])
+      expect(@user.errors.full_messages).to eq(["First name can't be blank", "Last name can't be blank"])
     end
     it "should give error if :email is not given" do
       @user = User.new({
-        :name => 'John Smith',
+        :first_name => 'John',
+        :last_name => 'Smith',
         :email => nil,
         :password => 'password',
         :password_confirmation => 'password'
@@ -33,14 +36,16 @@ RSpec.describe User, type: :model do
     end
     it "should not save to db if emails do not match even with different casing" do
       @user = User.new({
-        :name => 'John Smith',
+        :first_name => 'John',
+        :last_name => 'Smith',
         :email => 'john.smith@gmail.com',
         :password => 'password',
         :password_confirmation => 'password'
       })
       @user.save
       @user = User.new({
-        :name => 'John Smith',
+        :first_name => 'John',
+        :last_name => 'Smith',
         :email => 'JOHN.SMITH@gmail.com',
         :password => 'password',
         :password_confirmation => 'password'
@@ -49,7 +54,8 @@ RSpec.describe User, type: :model do
     end
     it "should give error if password or password_confirmation is not given" do
       @user = User.new({
-        :name => 'John Smith',
+        :first_name => 'John',
+        :last_name => 'Smith',
         :email => 'john.smith@gmail.com',
         :password =>nil,
         :password_confirmation => nil
@@ -59,7 +65,8 @@ RSpec.describe User, type: :model do
     end
     it "should not save if password and password_confirmation do not match" do
       @user = User.new({
-        :name => 'John Smith',
+        :first_name => 'John',
+        :last_name => 'Smith',
         :email => 'john.smith@gmail.com',
         :password =>'password',
         :password_confirmation => 'PASSWORD'
@@ -69,7 +76,8 @@ RSpec.describe User, type: :model do
     end
     it "should not save if password is shorter than 8 characters" do
       @user = User.new({
-        :name => 'John Smith',
+        :first_name => 'John',
+        :last_name => 'Smith',
         :email => 'john.smith@gmail.com',
         :password =>'toshort',
         :password_confirmation => 'toshort'
@@ -80,60 +88,37 @@ RSpec.describe User, type: :model do
   end
 
   describe '.authenticate_with_credentials' do
-    it "should allow user to login with proper credentials" do
-      @user = User.new({
-        :name => 'John Smith',
+
+    before(:each) do
+      @userInDB = User.new({
+        :first_name => 'John',
+        :last_name => 'Smith',
         :email => 'john.smith@gmail.com',
         :password =>'password',
         :password_confirmation => 'password'
       })
-      @user.save
+      @userInDB.save
+    end
+
+    it "should allow user to login with proper credentials" do
       user = User.authenticate_with_credentials('john.smith@gmail.com', 'password')
-      expect(user.name).to eq('John Smith')
+      expect(user.first_name).to eq('John')
     end
     it "should not allow user to login with invalid email" do
-      @user = User.new({
-        :name => 'John Smith',
-        :email => 'john.smith@gmail.com',
-        :password =>'password',
-        :password_confirmation => 'password'
-      })
-      @user.save
       user = User.authenticate_with_credentials('jason.smyth@gmail.com', 'password')
       expect(user).to eq(nil)
     end
     it "should not allow user to login with invalid password" do
-      @user = User.new({
-        :name => 'John Smith',
-        :email => 'john.smith@gmail.com',
-        :password =>'password',
-        :password_confirmation => 'password'
-      })
-      @user.save
       user = User.authenticate_with_credentials('john.smith@gmail.com', 'wordpass')
       expect(user).to eq(false)
     end
     it "should allow user to login with padded spacing around email" do
-      @user = User.new({
-        :name => 'John Smith',
-        :email => 'john.smith@gmail.com',
-        :password =>'password',
-        :password_confirmation => 'password'
-      })
-      @user.save
       user = User.authenticate_with_credentials('  john.smith@gmail.com  ', 'password')
-      expect(user.name).to eq("John Smith")
+      expect(user.first_name).to eq('John')
     end
     it "should allow user to login with the same email but different casing" do
-      @user = User.new({
-        :name => 'John Smith',
-        :email => 'john.smith@gmail.com',
-        :password =>'password',
-        :password_confirmation => 'password'
-      })
-      @user.save
       user = User.authenticate_with_credentials('John.Smith@gmail.com', 'password')
-      expect(user.name).to eq("John Smith")
+      expect(user.first_name).to eq('John')
     end
   end
 
